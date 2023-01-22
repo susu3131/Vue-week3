ab0f1341d05dbe0635bb95010547eaba3397a228
蘇蘇蘇前端之路 <105415618+susu3131@users.noreply.github.com>
2023-01-22 16:47:48 +0800

    登入ok

--- product.js
+++ product.js
@@ -1,1 +1,17 @@
-I
+import {createApp} from "https://unpkg.com/vue@3/dist/vue.esm-browser.js"
+
+createApp({
+  data(){
+    return{
+
+    }
+  },
+  methods: {
+    
+  },
+  mounted() {
+    console.log('test')
+  }
+
+
+}).mount('#app')
