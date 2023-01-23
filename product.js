59ed62180655ceb35f2082f27c27734814b01e75
蘇蘇蘇前端之路 <105415618+susu3131@users.noreply.github.com>
2023-01-23 14:23:36 +0800

    產品匯入

--- product.js
+++ product.js
@@ -7,10 +7,17 @@ const apiPath="susu3131"
 createApp({
   data(){
     return{
-
+      products:[ ],
+      tempProduct:{ }
     }
   },
   methods: {
+    getData(){
+      const url = `${apiUrl}v2/api/${apiPath}/admin/products/all`
+      axios.get(url)
+        .then(res => this.products = res.data.products)
+        .catch(err => console.log(err.data))
+    },
     checkLogin(){
       const url = `${apiUrl}v2/api/user/check`
       axios.post(url)
@@ -25,7 +32,8 @@ createApp({
     //驗證登入
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)restoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     axios.defaults.headers.common['Authorization'] = token;
-    this.checkLogin()
+    this.checkLogin();
+    this.getData();
   }
 
 
